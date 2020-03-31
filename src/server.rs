2e8788a7c5bbe36adadecc8359e403494ef5f41e
Maté Antunovic <just.mate.antunovic@gmail.com>
2020-03-31 19:31:50 +1300

    Finally implement a conversion future!

--- src/server.rs
+++ src/server.rs
@@ -3,9 +3,9 @@ use actix::Addr;
 use crate::rpc::Rpc;
 use actix_raft::messages;
 use crate::Data;
-use futures::future::Future;
-use core::pin::Pin;
 use actix::prelude::Request;
+use actix::prelude::Future as ActixFuture;
+use futures::Async;
 
 #[derive(Clone)]
 struct Server {
@@ -13,34 +13,25 @@ struct Server {
 }
 
 struct RpcFuture { 
-    //future: Box<dyn Future<Output=std::result::Result<R, E>>>
     request: Request<crate::AppRaft, messages::AppendEntriesRequest<Data>>//messages::AppendEntriesRequest<Data>>
 }
-
-//use futures::Async as FuturesAsync;
-use actix::prelude::Future as ActixFuture;
-
-use futures::Poll;
-use futures::Async;
-
-use std::task::Context;
 
 impl std::future::Future for RpcFuture { 
     type Output = Result<messages::AppendEntriesResponse, ()>;
 
-    fn poll(self: std::pin::Pin<&mut Self>, _context: &mut std::task::Context<'_>) -> std::task::Poll<std::result::Result<actix_raft::messages::AppendEntriesResponse, ()>> {
+    fn poll(mut self: std::pin::Pin<&mut Self>, _context: &mut std::task::Context<'_>) -> std::task::Poll<std::result::Result<actix_raft::messages::AppendEntriesResponse, ()>> {
         match ActixFuture::poll(&mut self.request) {
             Ok(Async::NotReady) => std::task::Poll::Pending,//Poll::Ready(obj), //Ok(obj),
             Ok(Async::Ready(val)) => std::task::Poll::Ready(val),//Poll::Ready(obj), //Ok(obj),
-            Err(err) => std::task::Poll::Ready(Err(()))
+            Err(_) => std::task::Poll::Ready(Err(()))
         }
     }
 }
 
 impl Rpc for Server {
-    type SendClientPayloadFut = RpcFuture;//<messages::AppendEntriesRequest<Data>>  //Request<crate::AppRaft, >; //RpcFuture<>;
+    type SendClientPayloadFut = RpcFuture;
 
-    fn send_client_payload(self, _: tarpc::context::Context,msg: messages::AppendEntriesRequest<Data>) -> Self::SendClientPayloadFut {
+    fn send_client_payload(self, _: tarpc::context::Context, _msg: messages::AppendEntriesRequest<Data>) -> Self::SendClientPayloadFut {
         unimplemented!()
     }
 }
